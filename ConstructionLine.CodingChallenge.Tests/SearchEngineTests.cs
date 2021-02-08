9956db53a130b022e6b49383c61bbbf5844e38ad
Anil <anil_bhatia2k@hotmail.com>
2021-02-08 12:21:46 +0000

    Add project files.

---
+++ ConstructionLine.CodingChallenge.Tests/SearchEngineTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace ConstructionLine.CodingChallenge.Tests
+{
+    [TestFixture]
+    public class SearchEngineTests : SearchEngineTestsBase
+    {
+
+        private List<Shirt> BuildTestShirts()
+        {
+            var shirts = new List<Shirt>
+            {
+                new Shirt(Guid.NewGuid(), "Red - Small", Size.Small, Color.Red),
+                new Shirt(Guid.NewGuid(), "Black - Medium", Size.Medium, Color.Black),
+                new Shirt(Guid.NewGuid(), "Blue - Large", Size.Large, Color.Blue),
+                new Shirt(Guid.NewGuid(), "Red - Medium", Size.Medium, Color.Red),
+                new Shirt(Guid.NewGuid(), "Red - Large", Size.Large, Color.Red),
+                new Shirt(Guid.NewGuid(), "Black - Small", Size.Small, Color.Black),
+                new Shirt(Guid.NewGuid(), "Black - Large", Size.Large, Color.Black),
+                new Shirt(Guid.NewGuid(), "Blue - Small", Size.Small, Color.Blue),
+                new Shirt(Guid.NewGuid(), "Blue - Medium", Size.Medium, Color.Blue),
+                new Shirt(Guid.NewGuid(), "Yellow - Medium", Size.Medium, Color.Yellow)
+            };
+            return shirts;
+        }
+
+        [Test]
+        public void Test()
+        {
+            var shirts = BuildTestShirts();
+
+            var searchEngine = new SearchEngine(shirts);
+
+            var searchOptions = new SearchOptions
+            {
+                Colors = new List<Color> {Color.Red},
+                Sizes = new List<Size> {Size.Small}
+            };
+
+            var results = searchEngine.Search(searchOptions);
+
+            AssertResults(results.Shirts, searchOptions);
+            AssertSizeCounts(shirts, searchOptions, results.SizeCounts);
+            AssertColorCounts(shirts, searchOptions, results.ColorCounts);
+        }
+
+        [Test]
+        public void TestWithMissingColor()
+        {
+            var shirts = BuildTestShirts();
+
+            var searchEngine = new SearchEngine(shirts);
+
+            var searchOptions = new SearchOptions
+            {               
+                Sizes = new List<Size> { Size.Small }
+            };
+
+            var results = searchEngine.Search(searchOptions);
+
+            AssertResults(results.Shirts, searchOptions);
+            AssertSizeCounts(shirts, searchOptions, results.SizeCounts);
+            AssertColorCounts(shirts, searchOptions, results.ColorCounts);
+        }
+
+        [Test]
+        public void TestWithMissingSize()
+        {
+            var shirts = BuildTestShirts();
+
+            var searchEngine = new SearchEngine(shirts);
+
+            var searchOptions = new SearchOptions
+            {
+                Colors = new List<Color> { Color.Blue }
+            };
+
+            var results = searchEngine.Search(searchOptions);
+
+            AssertResults(results.Shirts, searchOptions);
+            AssertSizeCounts(shirts, searchOptions, results.SizeCounts);
+            AssertColorCounts(shirts, searchOptions, results.ColorCounts);
+        }
+    }
+}
---
+++ ConstructionLine.CodingChallenge/SearchEngine.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ConstructionLine.CodingChallenge
+{
+    public class SearchEngine
+    {
+        private readonly List<Shirt> _shirts;
+        private Dictionary<string, List<Shirt>> dictShirts;
+
+        public SearchEngine(List<Shirt> shirts)
+        {
+            _shirts = shirts;
+            // TODO: data preparation and initialisation of additional data structures to improve performance goes here.
+            dictShirts = new Dictionary<string, List<Shirt>>();
+            shirts.ForEach(x => {
+                string key = $"{x.Color.Id} - {x.Size.Id}";
+                if (dictShirts.ContainsKey(key))
+                {
+                    dictShirts[key].Add(x);
+                }
+                else
+                {
+                    dictShirts.Add(key, new List<Shirt>() { x });
+                }
+            });          
+
+        }
+
+
+        public SearchResults Search(SearchOptions options)
+        {
+            // TODO: search logic goes here.
+            if (options.Colors == null || options.Colors.Count == 0)
+            {
+                options.Colors = Color.All;
+            }
+            if (options.Sizes == null || options.Sizes.Count == 0)
+            {
+                options.Sizes = Size.All;
+            }
+            List<Shirt> searchedShirts = new List<Shirt>();
+            var searchKeys = (from optsize in options.Sizes
+                             from optcolor in options.Colors
+                             select $"{optcolor.Id} - {optsize.Id}").ToList();
+
+
+            searchKeys.ForEach(x =>
+            { 
+               if (dictShirts.ContainsKey(x))
+               {
+                    searchedShirts.AddRange(dictShirts[x]);
+               }
+            
+            });
+
+            var colorCounts = getColorCounts(searchedShirts).ToList();
+            var sizeCounts = getSizeCounts(searchedShirts).ToList();
+
+            return new SearchResults
+            {
+                Shirts = searchedShirts,
+                ColorCounts = colorCounts,
+                SizeCounts = sizeCounts
+            };
+        }
+
+        private static IEnumerable<SizeCount> getSizeCounts(List<Shirt> searchedShirts)
+        {           
+            var sizeCounts = searchedShirts
+                .GroupBy(sh => sh.Size)
+                .Select(size => new SizeCount {
+                 Size = size.Key,
+                 Count = size.Count()                
+                });
+            return sizeCounts;          
+        }
+
+
+        private IEnumerable<ColorCount> getColorCounts(List<Shirt> searchedShirts)
+        {
+            var colorCounts = searchedShirts
+                .GroupBy(sh => sh.Color)
+                .Select(color => new ColorCount
+                {
+                    Color = color.Key,
+                    Count = color.Count()
+                });
+            return colorCounts;
+          
+        }
+    }
+}
